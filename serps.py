52b2ad1b9e79c8451d53857621c85c7bc9197496
Kenneth Cheong <clarinet.kenneth@gmail.com>
2022-07-22 18:12:48 +0800

    Update serps.py

--- serps.py
+++ serps.py
@@ -25,7 +25,7 @@ for option in options:
 
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
-keywords = ['seo marketing','sem marketing']
+keywords = ['sem marketing']
 
 targeted_url = 'mediaone'
 
@@ -33,36 +33,13 @@ ranking_list = []
 
 for i in keywords:
     i.replace(' ','+')
-    driver.get('https://www.google.com/search?q='+i+"&cr=countrysg&pws=0&num=100")
-    time.sleep(180)
-    
-    list_1 = []
-    list_2 = []
-    list_3 = []
+    driver.get('https://www.google.com/search?q='+i+"&cr=countrysg&pws=0&num=30")
+    time.sleep(30)
 
-    for i in range(1,101):
-        try:
-            domain = ((driver.find_element(By.XPATH,'//*[@id="rso"]/div['+str(i)+']/div/div[1]/div/a/div/cite').text).split(''))[0]
-            title = driver.find_element(By.XPATH,'//*[@id="rso"]/div['+str(i)+']/div/div[1]/div/a/h3').text
-            desc = driver.find_element(By.XPATH,'//*[@id="rso"]/div['+str(i)+']/div/div[2]').text
-            if desc == '':
-                 desc = driver.find_element(By.XPATH,'//*[@id="rso"]/div['+str(i)+']/div/div[3]').text
-            list_1.append(domain)
-            list_2.append(title)
-            list_3.append(desc)
-        except:
-            pass
-
-    df = pd.DataFrame(list(zip(list_1,list_2,list_3)), columns =['domain','meta_title','meta_desc'])
-    df.index += 1
-    
-    try:
-        ranking_list.append(((df.index[df['domain'].str.contains(targeted_url)]).tolist())[0])
-    except:
-        ranking_list.append('not found')
-
-df_kws_rankings = pd.DataFrame(list(zip(keywords,ranking_list)), columns =['keyword','position'])
-
-df_kws_rankings.to_csv(targeted_url+'_rankings.csv')
-
-df.to_csv(targeted_url+'_serps.csv')
+    for i in range(5,10):
+        domain = ((driver.find_element(By.XPATH,'//*[@id="rso"]/div['+str(i)+']/div/div[1]/div/a/div/cite').text).split(''))[0]
+        title = driver.find_element(By.XPATH,'//*[@id="rso"]/div['+str(i)+']/div/div[1]/div/a/h3').text
+        desc = driver.find_element(By.XPATH,'//*[@id="rso"]/div['+str(i)+']/div/div[2]').text
+        if desc == '':
+            desc = driver.find_element(By.XPATH,'//*[@id="rso"]/div['+str(i)+']/div/div[3]').text
+        print(domain,title,desc)
